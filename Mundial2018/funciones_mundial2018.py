adec3502e1b8e175e07ccc52b8fde2593e7a38a0
Javier Pérez Hidalgo <reyole111@gmail.com>
2020-03-03 19:02:17 +0100

    Función 1

--- Mundial2018/funciones_mundial2018.py
+++ Mundial2018/funciones_mundial2018.py
@@ -1,2 +1,11 @@
-.
+import json
 
+with open("mundial2018.json") as fichero:
+    datos=json.load(fichero)
+
+def listar_informacion(datos):
+    lista_selecciones=[]
+    for rondas in datos["rounds"]:
+        for seleccion in rondas["matches"]:
+            lista_selecciones.append(seleccion["team1"]["name"])
+    return lista_selecciones
