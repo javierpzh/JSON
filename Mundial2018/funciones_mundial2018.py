2b73a34c02ec8d0635753416974c7695d32b9677
Javier Pérez Hidalgo <reyole111@gmail.com>
2020-03-03 21:03:57 +0100

    Función 2

--- Mundial2018/funciones_mundial2018.py
+++ Mundial2018/funciones_mundial2018.py
@@ -9,3 +9,10 @@ def listar_informacion(datos):
         for seleccion in rondas["matches"]:
             lista_selecciones.append(seleccion["team1"]["name"])
     return lista_selecciones
+
+def contar_informacion(datos):
+    lista_selecciones=[]
+    for rondas in datos["rounds"]:
+        for seleccion in rondas["matches"]:
+            lista_selecciones.append(seleccion["team1"]["name"])
+    return len(lista_selecciones)
